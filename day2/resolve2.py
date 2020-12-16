963ef9fcb6d0fc7fe4ba7cc3898711b6479c1a37
Poloschki <patrick77290@gmail.com>
2020-12-16 15:21:51 +0100

    day 2

--- day2/resolve2.py
+++ day2/resolve2.py
@@ -10,13 +10,15 @@ for i in range(len(lines)):
     lower = tmp[0]   
     tmp = tmp[1].split(" ")
     pswd = tmp[2]
-    max = tmp[0]
+    higher = tmp[0]
     char = tmp[1][0]
 
-    for j in pswd:
-        if char in j : 
-            subcount += 1
-    if subcount >= int(lower) and subcount <= int(max):
-        count += 1
+    if char in pswd[int(lower)-1]:
+        subcount +=1 
+    if char in pswd[int(higher)-1]:
+        subcount +=1
+
+    if subcount == 1:
+        count += 1        
 
 print (count)
